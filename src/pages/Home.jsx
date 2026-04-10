a4c0daed31743f8e87e5f41a5b52e0a321ae1595
zack-das <izackdas@gmail.com>
2026-04-10 10:52:43 +0300

    Updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,12 +7,15 @@ function Home() {
         <div className="home-container">
           <div className="hero">
             <div className="heroH"><h2>NEW COLLECTION 2026</h2></div>
-            <h1>Fashion for<br/> Every Age & Style</h1>
-            <p>Discover the latest trends in clothing and <br/> accessories for women, men, kids, and teenagers.<br/> Quality meets style.</p>
+            
           </div>
+          <div className='herotag'>
+              <h1>Fashion for<br/> Every Age & Style</h1>
+            <p>Discover the latest trends in clothing and <br/> accessories for women, men, kids, and teenagers.<br/> <span>Quality</span> meets <span className='styled'><span className='s'>s</span><span className='s'>t</span><span className='y'>y</span><span className='l'>l</span><span className='e'>e</span>.</span></p>
+            </div>
            <div className="home-cta">
             <button className="hero-cta">Shop now</button>
-          <button className="hero-cta1">Latest offers</button>
+          <button className="hero-cta">Latest offers</button>
         </div>
         </div>
       </section>
